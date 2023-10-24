ec6db34d246a45ca206c6cd63573fa63ca7d5332
Edgar Huaman <rhum.sap@gmail.com>
2023-10-24 16:31:31 -0500

    end project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -96,9 +96,12 @@ export class AppComponent implements AfterViewInit,OnInit{
   //image dinamic
   imgePerfil:string='';
 
-  //reply
+  //reply Temp
   replyState:boolean=false;
   idUser:number=0;
+  indiceTmp:number=0;
+  idxTmp:number=0;
+
 
   replyComment='';
 
@@ -209,11 +212,18 @@ export class AppComponent implements AfterViewInit,OnInit{
     //console.log(this.calculateDiffTime(new Date().toISOString(),new Date().toISOString()));
   }
 
-  reply(id:number,username:string){
+  reply(indiceTmp:number,idxTmp:number,id:number,username:string,level:number){
     this.replyComment=`@${username}, `;
     if(this.idUser!==id){
       this.replyState=false;
       this.idUser=id;
+      if(level===0){
+        this.indiceTmp=indiceTmp;
+      }else if(level===1){
+        this.indiceTmp=indiceTmp;
+        this.idxTmp=idxTmp;
+        console.log('hh');
+      }
       this.replyState=true;
     }else{
       this.replyState=!this.replyState;
@@ -440,7 +450,6 @@ export class AppComponent implements AfterViewInit,OnInit{
   refreshTemplateTimes(ListaCommentArray:comment[]):void{
     this.showTime(ListaCommentArray);
     this.data.comments=[...ListaCommentArray];
-    console.log(this.data.comments);
   }
 
   formatoFecha(fecha:Date, formato:String) {
@@ -458,8 +467,124 @@ export class AppComponent implements AfterViewInit,OnInit{
   ngOnInit(){
     let ListaComment: string | null = localStorage.getItem("ListaComment");
     if(!ListaComment){
-      localStorage.setItem("ListaComment",JSON.stringify([]));
-      this.data.comments=[];
+      //localStorage.setItem("ListaComment",JSON.stringify([]));
+      localStorage.setItem("ListaComment",JSON.stringify([
+        {
+          "id": 1,
+          "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+          "createdAt": "1 month ago",
+          "score": 12,
+          "user": {
+            "image": {
+              "png": "../assets/images/avatars/image-amyrobson.png",
+              "webp": "../assets/images/avatars/image-amyrobson.webp"
+            },
+            "username": "amyrobson"
+          },
+          "replies": []
+        },
+        {
+          "id": 2,
+          "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+          "createdAt": "2 weeks ago",
+          "score": 5,
+          "user": {
+            "image": {
+              "png": "../assets/images/avatars/image-maxblagun.png",
+              "webp": "../assets/images/avatars/image-maxblagun.webp"
+            },
+            "username": "maxblagun"
+          },
+          "replies": [
+            {
+              "id": 3,
+              "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+              "createdAt": "1 week ago",
+              "score": 4,
+              "replyingTo": "maxblagun",
+              "user": {
+                "image": {
+                  "png": "../assets/images/avatars/image-ramsesmiron.png",
+                  "webp": "../assets/images/avatars/image-ramsesmiron.webp"
+                },
+                "username": "ramsesmiron"
+              }
+            },
+            {
+              "id": 4,
+              "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+              "createdAt": "2 days ago",
+              "score": 2,
+              "replyingTo": "ramsesmiron",
+              "user": {
+                "image": {
+                  "png": "../assets/images/avatars/image-juliusomo.png",
+                  "webp": "../assets/images/avatars/image-juliusomo.webp"
+                },
+                "username": "juliusomo"
+              }
+            }
+          ]
+        }
+      ]))
+      this.data.comments=[
+        {
+        "id": 1,
+        "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+        "createdAt": "1 month ago",
+        "score": 12,
+        "user": {
+          "image": {
+            "png": "../assets/images/avatars/image-amyrobson.png",
+            "webp": "../assets/images/avatars/image-amyrobson.webp"
+          },
+          "username": "amyrobson"
+        },
+        "replies": []
+      },
+      {
+        "id": 2,
+        "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+        "createdAt": "2 weeks ago",
+        "score": 5,
+        "user": {
+          "image": {
+            "png": "../assets/images/avatars/image-maxblagun.png",
+            "webp": "../assets/images/avatars/image-maxblagun.webp"
+          },
+          "username": "maxblagun"
+        },
+        "replies": [
+          {
+            "id": 3,
+            "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+            "createdAt": "1 week ago",
+            "score": 4,
+            "replyingTo": "maxblagun",
+            "user": {
+              "image": {
+                "png": "../assets/images/avatars/image-ramsesmiron.png",
+                "webp": "../assets/images/avatars/image-ramsesmiron.webp"
+              },
+              "username": "ramsesmiron"
+            }
+          },
+          {
+            "id": 4,
+            "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+            "createdAt": "2 days ago",
+            "score": 2,
+            "replyingTo": "ramsesmiron",
+            "user": {
+              "image": {
+                "png": "../assets/images/avatars/image-juliusomo.png",
+                "webp": "../assets/images/avatars/image-juliusomo.webp"
+              },
+              "username": "juliusomo"
+            }
+          }
+        ]
+      }];
     }else{
         const ListaCommentArray=JSON.parse(ListaComment);
         this.refreshTemplateTimes(ListaCommentArray);
@@ -467,7 +592,7 @@ export class AppComponent implements AfterViewInit,OnInit{
     }
 
   ngAfterViewInit(){
-    console.log('dh');
+
   }
 }
 
